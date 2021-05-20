603778c0422d6120254c028d282b845e94ea5494
Damen King <damking@adobe.com>
2021-05-19 21:57:47 -0700

    added some webpack

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function component() {
   const element = document.createElement('div');
   const btn = document.createElement('button');
 
-  element.innerHTML = 'Hello webpack';
+  element.innerHTML = 'Hello webpack!!!!!';
   element.classList.add('hello');
   btn.innerHTML = 'Click me and check the console!';
   btn.onclick = printMe;
